f5e8330aeb328060f3b8c4d6ab8ae9e748f420b4
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-11-24 20:26:01 -0800

    Modify Package.swift and update minimum swift-tools version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyShell",
+    products: [
+        .executable(name: "swftsh", targets: ["SwiftyShell"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -12,7 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "SwiftyShell",
             dependencies: []),
         .testTarget(
